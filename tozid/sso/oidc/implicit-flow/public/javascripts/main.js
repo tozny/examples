901d95e3f9ad200f0306255cd44daadae741d668
Levi Sky <levi@tozny.com>
2020-09-29 07:52:03 -0700

    Add example for oidc implict flow sso login using TozID.

---
+++ tozid/sso/oidc/implicit-flow/public/javascripts/main.js
@@ -0,0 +1,57 @@
+document.getElementById('login').addEventListener("click", redirectToLogin, false);
+
+Oidc.Log.logger = console;
+Oidc.Log.level = Oidc.Log.INFO;
+
+//
+// OIDC Client Configuration
+//
+const TOZID_CLIENT_ID = 'demo';
+const TOZID_HOSTNAME = 'https://id.tozny.com';
+
+var settings = {
+    authority: `${TOZID_HOSTNAME}/auth/realms/${TOZID_CLIENT_ID}/.well-known/openid-configuration`,
+    client_id: TOZID_CLIENT_ID,
+    redirect_uri: window.location.origin,
+    response_type: 'id_token token',
+    scope: 'openid profile',
+    filterProtocolClaims: true,
+    loadUserInfo: true
+};
+var mgr = new Oidc.UserManager(settings);
+
+//
+// Redirect to TozId to authenticate the user
+//
+function redirectToLogin(e) {
+  e.preventDefault();
+
+  mgr.signinRedirect({state:'some data'}).then(function() {
+      console.log("signinRedirect done");
+  }).catch(function(err) {
+      console.log(err);
+  });
+}
+
+//
+// Handle the authentication response returned
+// by TozId after the user has attempted to authenticate
+//
+function processLoginResponse() {
+  mgr.signinRedirectCallback().then(function(user) {
+      console.log("signed in", user);
+
+      document.getElementById("loginResult").innerHTML = '<h3>Success</h3><pre><code>' + JSON.stringify(user, null, 2) + '</code></pre>'
+
+  }).catch(function(err) {
+      console.log(err);
+  });
+}
+
+//
+// Look out for a authentication response
+// then log it and handle it
+//
+if (window.location.href.indexOf("#") >= 0) {
+  processLoginResponse();
+}
