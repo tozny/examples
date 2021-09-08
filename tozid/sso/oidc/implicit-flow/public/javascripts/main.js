5afe638c178971d23ba45809c675171e4265f13b
Eli Fabens <efabens@tozny.com>
2021-09-08 15:45:17 -0700

    [WR-138] Update app to support role checks
    - When user has admin role displays a button
    - Uses real_access.roles fields to validate, this is the default
    location of roles when "roles" scope is provided

--- tozid/sso/oidc/implicit-flow/public/javascripts/main.js
+++ tozid/sso/oidc/implicit-flow/public/javascripts/main.js
@@ -15,7 +15,7 @@ var settings = {
     client_id: TOZID_CLIENT_ID,
     redirect_uri: window.location.origin,
     response_type: 'id_token token',
-    scope: 'openid profile',
+    scope: 'openid profile roles',
     filterProtocolClaims: true,
     loadUserInfo: true
 };
@@ -40,13 +40,36 @@ function redirectToLogin(e) {
 //
 function processLoginResponse() {
   mgr.signinRedirectCallback().then(function(user) {
-      console.log("signed in", user);
-
       document.getElementById("loginResult").innerHTML = '<h3>Success</h3><pre><code>' + JSON.stringify(user, null, 2) + '</code></pre>'
+      maybeBuildAdminSection(user.profile)
 
   }).catch(function(err) {
       console.log(err);
   });
+}
+
+//
+// Update the admin section if the provided profile has an "admin" role
+//
+function maybeBuildAdminSection(profile) {
+    let roles = profile.realm_access?.roles;
+    if (Array.isArray(roles) && roles.includes("admin")) {
+        console.log(roles)
+        document.getElementById("adminOnly").innerHTML = '<p><a class="btn big-red" id="adminButton">ADMIN ONLY</a></p>'
+        document.getElementById("adminButton").addEventListener("click", popUpAlertAdmin, false);
+    } else {
+        console.log(profile.realm_access)
+        document.getElementById("adminOnly").innerHTML = '<h3>nothing to see here</h3>'
+    }
+}
+
+//
+// Create an alert, to be used in callbacks
+//
+function popUpAlertAdmin(e) {
+    e.preventDefault()
+    alert("You are an admin!")
+
 }
 
 //
